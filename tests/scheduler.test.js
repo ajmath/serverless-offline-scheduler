e2517527703df94d13b6f61406182e267dc3cbbc
Andrew Matheny <andrew.j.matheny@gmail.com>
2017-03-10 10:58:25 -0500

    Support for object schedule syntax

--- tests/scheduler.test.js
+++ tests/scheduler.test.js
@@ -41,15 +41,25 @@ describe("validate", () => {
   });
 
   it("should parse cron from cron event expression", () => {
-    let result = module._convertScheduleToCron("cron(1/* * * * *)");
+    let result = module._convertExpressionToCron("cron(1/* * * * *)");
     expect(result).to.eql("1/* * * * *");
 
-    result = module._convertScheduleToCron("cron(15 10 ? * 6L 2002-2005)");
+    result = module._convertExpressionToCron("cron(15 10 ? * 6L 2002-2005)");
     expect(result).to.eql("15 10 ? * 6L 2002-2005");
   });
 
+  it("should parse cron from object syntax", () => {
+    const toCron = (s) => module._parseScheduleObject("my-job", s);
+
+    expect(toCron({ rate: "rate(10 minutes)", enabled: true })).to.eql({
+      name: "my-job",
+      cron: "*/10 * * * *",
+      enabled: true
+    });
+  });
+
   it("should parse cron from rate event expression", () => {
-    const toCron = (s) => module._convertScheduleToCron(s);
+    const toCron = (s) => module._convertExpressionToCron(s);
 
     expect(toCron("rate(5 minutes)")).to.eql("*/5 * * * *");
     expect(toCron("rate(2 minute)")).to.eql("*/2 * * * *");
@@ -66,20 +76,35 @@ describe("validate", () => {
   });
 
   it("should return null if invalid event expression", () => {
-    const result = module._convertScheduleToCron("cronic(1/* * * * *)");
+    const result = module._convertExpressionToCron("cronic(1/* * * * *)");
     expect(result).to.eql(null);
   });
 
   it("should load functions with schedule events", () => {
     module.serverless.service.functions = {
       scheduled1: {
-        handler: "handler.js",
+        handler: "handler.test1",
         events: [{
           schedule: "cron(1/* * * * *)"
         }]
       },
+      scheduled2: {
+        handler: "handler.test2",
+        events: [
+          {
+            schedule: {
+              name: "custom-name",
+              rate: "rate(2 hours)",
+              enabled: false
+            }
+          },
+          {
+            schedule: "cron(1/* * * * *)"
+          }
+        ]
+      },
       http: {
-        handler: "handler.js",
+        handler: "handler.web",
         events: [{
           http: { method: "get", path: "path/123" }
         }]
@@ -87,8 +112,26 @@ describe("validate", () => {
     };
 
     const funcs = module._getFuncConfigs();
-
-    expect(funcs).to.have.lengthOf(1);
+    const expectedLength = 2;
+    expect(funcs).to.have.lengthOf(expectedLength);
     expect(funcs[0]).to.have.property("id").that.equals("scheduled1");
+    expect(funcs[0]).to.have.property("events");
+
+    const event1 = funcs[0].events[0];
+    expect(event1).to.have.property("name").that.equals("scheduled1");
+    expect(event1).to.have.property("enabled").that.equals(true);
+    expect(event1).to.have.property("cron").that.equals("1/* * * * *");
+
+    expect(funcs[1].events).to.have.lengthOf(expectedLength);
+
+    const event2 = funcs[1].events[0];
+    expect(event2).to.have.property("name").that.equals("scheduled2");
+    expect(event2).to.have.property("enabled").that.equals(true);
+    expect(event2).to.have.property("cron").that.equals("0 */2 * * *");
+
+    const event3 = funcs[1].events[1];
+    expect(event3).to.have.property("name").that.equals("scheduled2");
+    expect(event3).to.have.property("enabled").that.equals(true);
+    expect(event3).to.have.property("cron").that.equals("1/* * * * *");
   });
 });

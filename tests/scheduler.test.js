b272e7f7fd4381db62614ca748be7d3b134b95e1
Andrew Matheny <andrew.j.matheny@gmail.com>
2019-12-26 10:21:26 -0500

    Update to latest node and simplify lint/test dependencies

--- tests/scheduler.test.js
+++ tests/scheduler.test.js
@@ -1,37 +1,28 @@
 "use strict";
 /* eslint-env mocha */
 
-const chai = require("chai");
+const childProcess = require("child_process");
 const Serverless = require("serverless");
-const expect = chai.expect;
-
 const Scheduler = require("../lib/scheduler");
-const sinon = require("sinon");
-const BbPromise = require("bluebird");
-const childProcess = BbPromise.promisifyAll(require("child_process"));
 
 const MS_PER_SEC = 1000;
+
+jest.mock("child_process");
 
 describe("validate", () => {
   let module;
   let serverless;
-  let execFunction;
 
   beforeEach(() => {
     serverless = new Serverless();
     serverless.cli = {
       log: () => { }
     };
-    execFunction = sinon.stub(childProcess, "execSync")
-        .returns(BbPromise.resolve());
     module = new Scheduler(serverless);
   });
 
-  afterEach(() => {
-    childProcess.execSync.restore();
-  });
   it("should expose a `run` method", () => {
-    expect(module.run).to.be.a("function");
+    expect(typeof module.run).toEqual("function");
   });
 
   it("should not crash with no scheduled events", () => {
@@ -48,21 +39,21 @@ describe("validate", () => {
     };
 
     const funcs = module._getFuncConfigs();
-    expect(funcs).to.have.lengthOf(0);
+    expect(funcs).toHaveLength(0);
   });
 
   it("should parse cron from cron event expression", () => {
     let result = module._convertExpressionToCron("cron(1/* * * * *)");
-    expect(result).to.eql("1/* * * * *");
+    expect(result).toEqual("1/* * * * *");
 
     result = module._convertExpressionToCron("cron(15 10 ? * 6L 2002-2005)");
-    expect(result).to.eql("15 10 ? * 6L");
+    expect(result).toEqual("15 10 ? * 6L");
   });
 
   it("should parse cron from object syntax", () => {
     const toCron = (s) => module._parseScheduleObject("my-job", s);
 
-    expect(toCron({ rate: "rate(10 minutes)", enabled: true })).to.eql({
+    expect(toCron({ rate: "rate(10 minutes)", enabled: true })).toEqual({
       name: "my-job",
       ruleName: "my-job",
       cron: "*/10 * * * *",
@@ -73,23 +64,23 @@ describe("validate", () => {
   it("should parse cron from rate event expression", () => {
     const toCron = (s) => module._convertExpressionToCron(s);
 
-    expect(toCron("rate(5 minutes)")).to.eql("*/5 * * * *");
-    expect(toCron("rate(2 minute)")).to.eql("*/2 * * * *");
+    expect(toCron("rate(5 minutes)")).toEqual("*/5 * * * *");
+    expect(toCron("rate(2 minute)")).toEqual("*/2 * * * *");
 
-    expect(toCron("rate(1 hours)")).to.eql("0 */1 * * *");
-    expect(toCron("rate(6 hour)")).to.eql("0 */6 * * *");
+    expect(toCron("rate(1 hours)")).toEqual("0 */1 * * *");
+    expect(toCron("rate(6 hour)")).toEqual("0 */6 * * *");
 
-    expect(toCron("rate(3 day)")).to.eql("0 0 */3 * *");
-    expect(toCron("rate(9 days)")).to.eql("0 0 */9 * *");
+    expect(toCron("rate(3 day)")).toEqual("0 0 */3 * *");
+    expect(toCron("rate(9 days)")).toEqual("0 0 */9 * *");
 
-    expect(toCron("rate(7 year)")).to.eql(null);
-    expect(toCron("rate(3 lightyears)")).to.eql(null);
-    expect(toCron("rate(9 seconds)")).to.eql(null);
+    expect(toCron("rate(7 year)")).toEqual(null);
+    expect(toCron("rate(3 lightyears)")).toEqual(null);
+    expect(toCron("rate(9 seconds)")).toEqual(null);
   });
 
   it("should return null if invalid event expression", () => {
     const result = module._convertExpressionToCron("cronic(1/* * * * *)");
-    expect(result).to.eql(null);
+    expect(result).toEqual(null);
   });
 
   it("should load functions with schedule events", () => {
@@ -125,27 +116,27 @@ describe("validate", () => {
 
     const funcs = module._getFuncConfigs();
     const expectedLength = 2;
-    expect(funcs).to.have.lengthOf(expectedLength);
-    expect(funcs[0]).to.have.property("id").that.equals("scheduled1");
-    expect(funcs[0]).to.have.property("events");
+    expect(funcs).toHaveLength(expectedLength);
+    expect(funcs[0].id).toEqual("scheduled1");
+    expect(Array.isArray(funcs[0].events)).toEqual(true);
 
     const event1 = funcs[0].events[0];
-    expect(event1).to.have.property("name").that.equals("scheduled1");
-    expect(event1).to.have.property("enabled").that.equals(true);
-    expect(event1).to.have.property("cron").that.equals("1/* * * * *");
+    expect(event1.name).toEqual("scheduled1");
+    expect(event1.enabled).toEqual(true);
+    expect(event1.cron).toEqual("1/* * * * *");
 
-    expect(funcs[1].events).to.have.lengthOf(expectedLength);
+    expect(funcs[1].events).toHaveLength(expectedLength);
 
     const event2 = funcs[1].events[0];
-    expect(event2).to.have.property("name").that.equals("scheduled2");
-    expect(event2).to.have.property("enabled").that.equals(false);
-    expect(event2).to.have.property("cron").that.equals("0 */2 * * *");
-    expect(event2).to.have.property("ruleName").that.equals("custom-name");
+    expect(event2.name).toEqual("scheduled2");
+    expect(event2.enabled).toEqual(false);
+    expect(event2.cron).toEqual("0 */2 * * *");
+    expect(event2.ruleName).toEqual("custom-name");
 
     const event3 = funcs[1].events[1];
-    expect(event3).to.have.property("name").that.equals("scheduled2");
-    expect(event3).to.have.property("enabled").that.equals(true);
-    expect(event3).to.have.property("cron").that.equals("1/* * * * *");
+    expect(event3.name).toEqual("scheduled2");
+    expect(event3.enabled).toEqual(true);
+    expect(event3.cron).toEqual("1/* * * * *");
   });
 
   it("should load functions with schedule events", () => {
@@ -165,15 +156,15 @@ describe("validate", () => {
 
     const funcs = module._getFuncConfigs();
 
-    expect(funcs[0]).to.have.property("id").that.equals("scheduled1");
-    expect(funcs[0]).to.have.property("events");
+    expect(funcs[0].id).toEqual("scheduled1");
+    expect(Array.isArray(funcs[0].events)).toEqual(true);
 
-    expect(funcs[0].events).to.have.lengthOf(1);
+    expect(funcs[0].events).toHaveLength(1);
 
     const event = funcs[0].events[0];
-    expect(event).to.have.property("cron").that.equals("1/* * * * *");
-    expect(event).to.have.property("input");
-    expect(event.input).to.have.property("key1").that.equals("value1");
+    expect(event.cron).toEqual("1/* * * * *");
+    expect(event.input).toBeDefined();
+    expect(event.input.key1).toEqual("value1");
   });
 
   it("should run function with schedule events and inputf", () => {
@@ -193,21 +184,19 @@ describe("validate", () => {
 
     const funcs = module._getFuncConfigs();
 
-    expect(funcs[0]).to.have.property("id").that.equals("scheduled1");
-    expect(funcs[0]).to.have.property("events");
-
-    expect(funcs[0].events).to.have.lengthOf(1);
+    expect(funcs[0].id).toEqual("scheduled1");
+    expect(Array.isArray(funcs[0].events)).toEqual(true);
+    expect(funcs[0].events).toHaveLength(1);
 
     const event = funcs[0].events[0];
     module._executeFunction(funcs[0].id, event.input);
 
-    expect(event).to.have.property("cron").that.equals("1/* * * * *");
-    expect(event).to.have.property("input");
-    expect(event.input).to.have.property("key1").that.equals("value1");
-    expect(execFunction.calledWithExactly(
+    expect(event.cron).toEqual("1/* * * * *");
+    expect(event.input.key1).toEqual("value1");
+    expect(childProcess.execSync).toBeCalledWith(
       `serverless invoke local --function ${funcs[0].id} --data ${JSON.stringify(event.input)}`,
-       {cwd: "./", stdio: "inherit" }
-      )).to.equal(true);
+      {cwd: "./", stdio: "inherit" }
+    );
   });
 
   it("should use the *function* timeout for getRemainingTimeInMillis", () => {
@@ -229,8 +218,8 @@ describe("validate", () => {
 
     const funcs = module._getFuncConfigs();
     const context = module._getContext(funcs[0]);
-    expect(context.getRemainingTimeInMillis()).to.be.at.most(timeout * MS_PER_SEC);
-    expect(context.getRemainingTimeInMillis()).to.be.at.least(timeout * MS_PER_SEC - maxDuration);
+    expect(context.getRemainingTimeInMillis()).toBeLessThanOrEqual(timeout * MS_PER_SEC);
+    expect(context.getRemainingTimeInMillis()).toBeGreaterThan(timeout * MS_PER_SEC - maxDuration);
   });
 
   it("should use the *provider* timeout for getRemainingTimeInMillis", () => {
@@ -252,8 +241,8 @@ describe("validate", () => {
 
     const funcs = module._getFuncConfigs();
     const context = module._getContext(funcs[0]);
-    expect(context.getRemainingTimeInMillis()).to.be.at.most(timeout * MS_PER_SEC);
-    expect(context.getRemainingTimeInMillis()).to.be.at.least(timeout * MS_PER_SEC - maxDuration);
+    expect(context.getRemainingTimeInMillis()).toBeLessThanOrEqual(timeout * MS_PER_SEC);
+    expect(context.getRemainingTimeInMillis()).toBeGreaterThan(timeout * MS_PER_SEC - maxDuration);
   });
 
   it("should use the *default* timeout for getRemainingTimeInMillis", () => {
@@ -274,7 +263,7 @@ describe("validate", () => {
 
     const funcs = module._getFuncConfigs();
     const context = module._getContext(funcs[0]);
-    expect(context.getRemainingTimeInMillis()).to.be.at.most(timeout * MS_PER_SEC);
-    expect(context.getRemainingTimeInMillis()).to.be.at.least(timeout * MS_PER_SEC - maxDuration);
+    expect(context.getRemainingTimeInMillis()).toBeLessThanOrEqual(timeout * MS_PER_SEC);
+    expect(context.getRemainingTimeInMillis()).toBeGreaterThan(timeout * MS_PER_SEC - maxDuration);
   });
 });
